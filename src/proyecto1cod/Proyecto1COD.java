807a73b0d10ff94482437cf8bb55e7374a0fedcf
jdominguezdominguez <jdominguezdominguez@victoria28.danielcastelao.org>
2017-10-05 09:43:44 +0200

    Texto cambiado a Hola Mundo

--- src/proyecto1cod/Proyecto1COD.java
+++ src/proyecto1cod/Proyecto1COD.java
@@ -7,7 +7,7 @@ public class Proyecto1COD {
     
     public static void main(String[] args) {
       
-        System.out.println("Hello World");
+        System.out.println("Hola Mundo");
         
     }
     
