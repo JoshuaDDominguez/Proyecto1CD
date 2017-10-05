648f89eab4dee1daa7043a7b4f04890dc4e46f5a
jdominguezdominguez <jdominguezdominguez@victoria28.danielcastelao.org>
2017-10-05 09:38:23 +0200

    Inicio proyecto

---
+++ src/proyecto1cod/Proyecto1COD.java
@@ -0,0 +1,14 @@
+
+package proyecto1cod;
+
+
+public class Proyecto1COD {
+
+    
+    public static void main(String[] args) {
+      
+        System.out.println("Hello World");
+        
+    }
+    
+}
